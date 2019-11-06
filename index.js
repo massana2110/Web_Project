49b3e2cdb52af8a643c7721cd25663be1141fb74
David Massana <00072416@uca.edu.sv>
2019-11-05 18:58:40 -0600

    Subiendo rama dev y añadiendo carpeta de rutas, manejo de views con pug

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Module Dependencies
+ */
+var express = require('express'); 
+var http = require('http');
+var path = require('path');
+
+var indexRouter = require('./routes/index');
+
+var app = express(); 
+
+/**
+ * Settings
+ */
+app.set('port', process.env.PORT || 3000); //Seteo el numero de puerto
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.get('/', function (req, res) {
+  	res.render("index")
+});
+
+//Listening the server
+http.createServer(app).listen(app.get("port"), function () {
+	console.log("Express server listening on port " + app.get("port"));
+})
