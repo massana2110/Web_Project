eb93092c3aea74b5bd0e5701cd3f6c16fdc715dd
RSanchez19 <00191018@uca.edu.sv>
2019-11-20 09:02:10 -0600

    Añadiendo validaciones a reserva

--- routes/add_reservation.js
+++ routes/add_reservation.js
@@ -7,8 +7,17 @@ router.get('/reservaciones', (req,res,next) =>{
 })
 
 router.post('/reservaciones/nueva-reservacion',(req,res)=>{
-    console.log(req.body)
-    res.send('Recibido')
+    const {fisrtname , lastname, telephone, email, arrive_date, departure_date, chosen_room, chosen_package} = req.body;
+    const errors = [];
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+    if(departure_date <= arrive_date){
+        errors.push({text: 'La fecha de salida no puede ser menor a la de arrivo'})
+    }
+    if(errors.length > 0){
+        res.render('add_reservation', {errors, email})
+    }else{
+        res.send('Recibido')
+    }
 })
 
 module.exports = router;
