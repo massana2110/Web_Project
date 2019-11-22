e1ce7668e5cce988361e4a4127662528d7c1a6a7
RSanchez19 <00191018@uca.edu.sv>
2019-11-21 19:55:32 -0600

    Arreglando responsividad y validados mensajes de error en reserva

--- routes/add_reservation.js
+++ routes/add_reservation.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const Reservation = require('../models/Reservation');
 
 /*GET home page */
@@ -8,15 +7,24 @@ router.get('/reservaciones', (req,res,next) =>{
     res.render('add_reservation', { title: 'Reservacion' });
 })
 
+
 router.post('/reservaciones/nueva-reservacion',async (req,res)=>{
     const {firstname , lastname, phone, email, arrive_date, departure_date, room, package} = req.body;
     const errors = [];
-    
-    if(arrive_date <= departure_date){
-        errors.push({text: 'La fecha de salida no puede ser menor a la de arribo'})
+    const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    const phoneRegexp = /^\d{8}-\d{1}$/;
+    if(phoneRegexp.test(phone) != true){
+        errors.push({text: "Nùmero de telefono incorrecto."})
+    }
+    if(emailRegexp.test(email)!= true){
+        errors.push({text: "Correo electronico incorrecto."})
+    }
+    if(departure_date <= arrive_date){
+        errors.push({text: 'La fecha de salida no puede ser menor o igual a la fecha de llegada.'})
     }
     if(errors.length > 0){
-        res.render('add_reservation', {errors, arrive_date, departure_date})
+        res.render('add_reservation', {errors, phone, email, arrive_date,
+             departure_date, room, package})
     }else{
         const newReservation = new Reservation({firstname, lastname, phone, email,
         arrive_date,departure_date,room,package});
