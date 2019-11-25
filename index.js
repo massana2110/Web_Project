cb38faddf42bd055aedb0a5485476d6f6d26b246
David Massana <00072416@uca.edu.sv>
2019-11-24 23:20:14 -0600

    Modificando la barra de navegacion para que sea dinamica

--- index.js
+++ index.js
@@ -34,14 +34,13 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
 
 /**
  * Global Variables
  */
-
 app.use((req,res,next) => {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
