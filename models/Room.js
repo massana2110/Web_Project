f2b1a7882adf7874adce2cef78a5a0e29c092279
David Massana <00072416@uca.edu.sv>
2019-11-26 14:04:54 -0600

    Añadiendo vista de instalaciones

--- models/Room.js
+++ models/Room.js
@@ -6,7 +6,7 @@ RoomSchema = new Schema({
     typeRoom: {type: String, required: true},
     correlative: {type: String, required: true},
     cost: {type: Number, required: true},
-    isAvailable: {type: Boolean, required: true},
+    isAvailable: {type: Boolean, required: true, default: true},
 });
 
 module.exports = mongoose.model('rooms', RoomSchema);
