b48dae1e9edaea4a39d272e7f93c449618191aae
massana2110 <00072416@uca.edu.sv>
2021-08-10 16:42:12 -0600

    fix style of reservations

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect( process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true ,
+    useCreateIndex: true,
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err))
